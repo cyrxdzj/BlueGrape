75cec566f02eb3ad4106a41d3e9fc69357411583
残阳如血 <2824671438@qq.com>
2022-01-29 18:43:18 +0800

    新增2个Activity，并完善我的壁纸页面。

---
+++ app/src/main/java/dzj/cyrxdzj/bluegrape/AboutThisSoftware.java
@@ -0,0 +1,14 @@
+package dzj.cyrxdzj.bluegrape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutThisSoftware extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_this_software);
+    }
+}
--- app/src/main/java/dzj/cyrxdzj/bluegrape/MainActivity.java
+++ app/src/main/java/dzj/cyrxdzj/bluegrape/MainActivity.java
@@ -6,17 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //新建BlueGrape文件夹
+        File folder=new File("/storage/emulated/0/BlueGrape");
+        if(!folder.exists())
+        {
+            folder.mkdir();
+            File file=new File("/storage/emulated/0/BlueGrape/current_wallpaper");
+            if(!file.exists())
+            {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
+        }
     }
     public void open_my_wallpaper(View view)
     {
         Intent intent=new Intent();
         intent.setClass(MainActivity.this,MyWallpaper.class);
+        MainActivity.this.startActivity(intent);
+    }
+    public void open_current_wallpaper(View view)
+    {
+        Intent intent=new Intent();
+        intent.setClass(MainActivity.this,CurrentWallpaper.class);
+        MainActivity.this.startActivity(intent);
+    }
+    public void open_about_this_software(View view)
+    {
+        Intent intent=new Intent();
+        intent.setClass(MainActivity.this,AboutThisSoftware.class);
         MainActivity.this.startActivity(intent);
     }
 }
