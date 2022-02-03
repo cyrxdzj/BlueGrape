bc11b2f0bb9f41fc1789555200bdb8ab9a69968c
残阳如血 <2824671438@qq.com>
2022-02-03 17:05:19 +0800

    完善“关于本软件”。

--- app/src/main/java/dzj/cyrxdzj/bluegrape/AboutThisSoftware.java
+++ app/src/main/java/dzj/cyrxdzj/bluegrape/AboutThisSoftware.java
@@ -3,8 +3,13 @@ package dzj.cyrxdzj.bluegrape;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AboutThisSoftware extends AppCompatActivity {
 
@@ -12,6 +17,22 @@ public class AboutThisSoftware extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_this_software);
+        try {
+            TextView app_version=(TextView)findViewById(R.id.app_version);
+            app_version.setText("Version "+String.valueOf(this.getPackageManager().getPackageInfo(this.getPackageName(),0).versionName));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public void reward_and_contribution(View view)
+    {
+        Intent intent=new Intent();
+        intent.setClass(this,RewardAndContribution.class);
+        this.startActivity(intent);
+    }
+    public void check_update(View view)
+    {
+        Toast.makeText(this,"无更新",Toast.LENGTH_SHORT).show();
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
