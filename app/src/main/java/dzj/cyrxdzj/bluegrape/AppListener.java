9b264a1a9d525fc603e288d232d0bb9b871a83e3
残阳如血 <2824671438@qq.com>
2022-04-10 22:23:09 +0800

    commit

--- app/src/main/java/dzj/cyrxdzj/bluegrape/AppListener.java
+++ app/src/main/java/dzj/cyrxdzj/bluegrape/AppListener.java
@@ -65,15 +65,15 @@ public class AppListener extends AccessibilityService {
     }
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        String packageName = event.getPackageName().toString();
+        String packageName = event.getPackageName().toString(), className = event.getClassName().toString();
         int eventType = event.getEventType();
-        Log.d("AppListener", "Event infomation: "+"packageName = " + packageName+" eventType = "+eventType + " eventTypeByString = " + AccessibilityEvent.eventTypeToString(eventType)+" eventClass = "+event.getClassName());
+        Log.d("AppListener", "Event infomation: "+"packageName = " + packageName+" eventType = "+eventType + " eventTypeByString = " + AccessibilityEvent.eventTypeToString(eventType)+" eventClass = "+className);
         Log.d("AppListener", "Now Activity class name: "+((ActivityManager)getSystemService(this.ACTIVITY_SERVICE)).getRunningTasks(1).get(0).topActivity.getClassName());
         if(isInputMethodApp(this,packageName))
         {
             return;
         }
-        if(event.getClassName().equals("com.android.server.am.AppErrorDialog"))
+        if(className.equals("com.android.server.am.AppErrorDialog"))
         {
             return;
         }
